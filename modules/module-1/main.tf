c764ae07e5f540e6a26b09c9eaf28fcdc1c82362
ylhan237 <ylhanfouossue@gmail.com>
2026-02-09 04:34:25 -0500

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -3598,7 +3598,7 @@ data "aws_ami" "goat_ami" {
 
 resource "aws_instance" "goat_instance" {
   ami                  = data.aws_ami.goat_ami.id
-  instance_type        = "t2.micro"
+  instance_type        = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.goat_iam_profile.name
   subnet_id            = aws_subnet.goat_subnet.id
   security_groups      = [aws_security_group.goat_sg.id]
